163a6203ce275986767827d228e0dfdb6d6f6674
micmine <micmine4@gmail.com>
2019-10-26 15:56:01 +0200

    Remove full path

--- start-docker.sh
+++ start-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # start test server
-docker run --rm --name lamp -p 8080:80 -e LOG_STDOUT=true -e LOG_STDERR=true -e LOG_LEVEL=debug -v /data/home/michael/Documents/php/simplelogin:/var/www/html fauria/lamp
+docker run --rm --name lamp -p 8080:80 -e LOG_STDOUT=true -e LOG_STDERR=true -e LOG_LEVEL=debug -v ~/Documents/php/simplelogin:/var/www/html fauria/lamp
 
