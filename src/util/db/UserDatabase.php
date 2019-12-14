e341007352ffc30e0f5cf60e7ade18dc93016b03
micmine <micmine4@gmail.com>
2019-12-14 09:17:39 +0100

    Add more isset checks

--- src/util/db/UserDatabase.php
+++ src/util/db/UserDatabase.php
@@ -2,69 +2,78 @@
 require "Db.php";
 
 function verifyPasswordHash($username, $password) {
-    $conn = connectDb();
+    if (isset($username) && $username != "") {
+        if (isset($password) && $password != "") {
+            $conn = connectDb();
 
-    $uid = "";
-    $password = mysqli_real_escape_string($conn, trim(htmlentities($password)));
-    $username = mysqli_real_escape_string($conn, trim(htmlentities($username)));
-    $query = "select uid, password from user where username = '$username' and isdeactivated = 0";
-    if ($result = mysqli_query($conn, $query)) {
-        $count = 0;
-
-        /* fetch associative array */
-        while ($data = mysqli_fetch_assoc($result)) {
-            $passwordHash = $data["password"];
-            if ($count <= 0 && password_verify($password, $passwordHash)) {                
-                // password is correct
-                $uid = $data["uid"];
-                $count = $count + 1;
+            $uid = "";
+            $password = mysqli_real_escape_string($conn, trim(htmlentities($password)));
+            $username = mysqli_real_escape_string($conn, trim(htmlentities($username)));
+            $query = "select uid, password from user where username = '$username' and isdeactivated = 0";
+            if ($result = mysqli_query($conn, $query)) {
+                $count = 0;
+            
+                /* fetch associative array */
+                while ($data = mysqli_fetch_assoc($result)) {
+                    $passwordHash = $data["password"];
+                    if ($count <= 0 && password_verify($password, $passwordHash)) {                
+                        // password is correct
+                        $uid = $data["uid"];
+                        $count = $count + 1;
+                    }
+                }
+            
+                mysqli_free_result($result);
             }
+            disconnectDb($conn);
         }
-    
-        mysqli_free_result($result);
     }
-
-    disconnectDb($conn);
     return $uid;
 }
 
 function getName($uid) { 
-    $conn = connectDb();
-    $name = "";
+    if (isset($uid) && $uid != "") {
+        $conn = connectDb();
+        $name = "";
 
-    $uid = mysqli_real_escape_string($conn, trim(htmlentities($uid)));
-    $query = "select username from user where uid = '$uid'";
-    if ($result = mysqli_query($conn, $query)) {
+        $uid = mysqli_real_escape_string($conn, trim(htmlentities($uid)));
+        $query = "select username from user where uid = '$uid'";
+        if ($result = mysqli_query($conn, $query)) {
 
-        /* fetch associative array */
-        while ($data = mysqli_fetch_assoc($result)) {
-            $name = $data["username"];
+            /* fetch associative array */
+            while ($data = mysqli_fetch_assoc($result)) {
+                $name = $data["username"];
+            }
+        
+            mysqli_free_result($result);
         }
-    
-        mysqli_free_result($result);
-    }
 
-    disconnectDb($conn);
+        disconnectDb($conn);
+    }
     return $name;
 }
 
 function setPassword($uid, $oldpassword, $newpassword) {
-    $oldpassword = mysqli_real_escape_string($conn, trim(htmlentities($oldpassword)));
-    $uid = verifyPasswordHash(getName($uid), $oldpassword);
     if (isset($uid) && $uid != "") {
-        $conn = connectDb();
+        if (isset($oldpassword) && $oldpassword != "") {
+            if (isset($newpassword) && $newpassword != "") {
+                $oldpassword = mysqli_real_escape_string($conn, trim(htmlentities($oldpassword)));
+                $uid = verifyPasswordHash(getName($uid), $oldpassword);
+                $conn = connectDb();
 
-        $newpassword = mysqli_real_escape_string($conn, trim(htmlentities($newpassword)));
-        $uid = mysqli_real_escape_string($conn, trim(htmlentities($uid)));
+                $newpassword = mysqli_real_escape_string($conn, trim(htmlentities($newpassword)));
+                $uid = mysqli_real_escape_string($conn, trim(htmlentities($uid)));
 
-        $hash = password_hash($newpassword, PASSWORD_DEFAULT);
+                $hash = password_hash($newpassword, PASSWORD_DEFAULT);
 
-        $query = "update user set password = '$hash' where uid = '$uid'";
-        if(!mysqli_query($conn, $query)){ 
-            error_log(mysqli_error($link)); 
+                $query = "update user set password = '$hash' where uid = '$uid'";
+                if(!mysqli_query($conn, $query)){ 
+                    error_log(mysqli_error($link)); 
+                }
+
+                disconnectDb($conn);
+            }
         }
-
-        disconnectDb($conn);
     }
     return $uid;
 }
